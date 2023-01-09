8819cb57aebd12533b28a6bda7f4b48f7a9a6e8c
Guigghisi <guilhermeghisi14@gmail.com>
2023-01-09 15:07:37 -0300

    Inicializando projeto

---
+++ src/test/java/com/devinhouse/pharmacymanagement/PharmacyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinhouse.pharmacymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
