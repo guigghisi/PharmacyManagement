4c38ac14891d3ea3202e6832304a4b956d36d3cc
Guigghisi <guilhermeghisi14@gmail.com>
2023-01-09 17:49:17 -0300

    Crud Medicamento

--- src/main/java/com/devinhouse/pharmacymanagement/controller/MedicamentoController.java
+++ src/main/java/com/devinhouse/pharmacymanagement/controller/MedicamentoController.java
@@ -1,15 +1,57 @@
 package com.devinhouse.pharmacymanagement.controller;
 
+import com.devinhouse.pharmacymanagement.entity.dto.MedicamentoDto;
 import com.devinhouse.pharmacymanagement.service.MedicamentoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/Medicamentos")
+@RequestMapping("/medicamentos")
 public class MedicamentoController {
     private final MedicamentoService medicamentoService;
 
     public MedicamentoController(MedicamentoService medicamentoService) {
         this.medicamentoService = medicamentoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MedicamentoDto cadastraMedicamento(@RequestBody MedicamentoDto medicamentoDto) {
+        var medicamento = medicamentoDto.transformarEmEntidade();
+        var medicamentoSalvo = medicamentoService.criarNovoMedicamento(medicamento);
+
+        return medicamentoDto.transformarEmDto(medicamentoSalvo);
+    }
+
+
+    @PutMapping("/{codigoMedicamento}")
+    @ResponseStatus(HttpStatus.OK)
+    public MedicamentoDto atualizarMedicamento(@PathVariable(value = "codigoMedicamento") Long codigoMedicamento, @RequestBody MedicamentoDto medicamentoDto) {
+        var medicamentoAtualizado = medicamentoService.atualizarMedicamento(codigoMedicamento, medicamentoDto.transformarEmEntidade());
+        return medicamentoDto.transformarEmDto(medicamentoAtualizado);
+    }
+
+    @GetMapping("/{codigoMedicamento}")
+    @ResponseStatus(HttpStatus.OK)
+    public MedicamentoDto consulta(@PathVariable(value = "codigoMedicamento") Long codigoMedicamento) {
+        var medicamentos = medicamentoService.buscarPeloId(codigoMedicamento);
+        var medicamentoDto = new MedicamentoDto();
+        return medicamentoDto.transformarEmDto(medicamentos);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<MedicamentoDto> consulta() {
+        var medicamentos = medicamentoService.buscarTodos();
+        var medicamentoDto = new MedicamentoDto();
+        return medicamentoDto.trasnformarEmListaDeDtos(medicamentos);
+    }
+
+    @DeleteMapping("/{codigoMedicamento}")
+    @ResponseStatus(HttpStatus.OK)
+    public void excluirMedicamento(@PathVariable(value = "codigoMedicamento") Long codigoMedicamento) {
+        medicamentoService.excluirMedicamento(codigoMedicamento);
     }
 }
--- src/main/java/com/devinhouse/pharmacymanagement/service/MedicamentoService.java
+++ src/main/java/com/devinhouse/pharmacymanagement/service/MedicamentoService.java
@@ -1,7 +1,12 @@
 package com.devinhouse.pharmacymanagement.service;
 
+import com.devinhouse.pharmacymanagement.entity.Medicamento;
+import com.devinhouse.pharmacymanagement.exception.MedicamentoNaoEncontradoException;
+import com.devinhouse.pharmacymanagement.exception.NenhumMedicamentoException;
 import com.devinhouse.pharmacymanagement.repository.MedicamentoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MedicamentoService {
@@ -9,5 +14,35 @@ public class MedicamentoService {
 
     public MedicamentoService(MedicamentoRepository repository) {
         this.repository = repository;
+    }
+
+    public Medicamento criarNovoMedicamento(Medicamento medicamento) {
+        return repository.save(medicamento);
+    }
+
+    public Medicamento atualizarMedicamento(Long codigoMedicamento, Medicamento medicamento) {
+        var medicamentoAntigo = repository.findById(codigoMedicamento).orElseThrow(MedicamentoNaoEncontradoException::new);
+
+        medicamentoAntigo.setDescricao_medicamento(medicamento.getDescricao_medicamento());
+        medicamentoAntigo.setNome_medicamento(medicamento.getNome_medicamento());
+        medicamentoAntigo.setDosagem_medicamento(medicamento.getDosagem_medicamento());
+        medicamentoAntigo.setTipo_medicamento(medicamento.getTipo_medicamento());
+        return repository.save(medicamentoAntigo);
+    }
+
+    public List<Medicamento> buscarTodos() {
+        if (repository.findAll().isEmpty()) {
+            throw new NenhumMedicamentoException();
+        }
+        return repository.findAll();
+    }
+
+    public Medicamento buscarPeloId(Long codigoMedicamento) {
+        return repository.findById(codigoMedicamento).orElseThrow(MedicamentoNaoEncontradoException::new);
+    }
+
+    public void excluirMedicamento(Long codigoMedicamento) {
+        var medicamento = repository.findById(codigoMedicamento).orElseThrow(MedicamentoNaoEncontradoException::new);
+        repository.delete(medicamento);
     }
 }
